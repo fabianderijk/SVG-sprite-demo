5a5e22eed9db1979163c9a688961edc2d2493317
Fabian de Rijk <f.de.rijk@youwe.nl>
2018-03-09 11:56:29 +0100

    Changes based on writing the blog

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var debug = require('gulp-debug');
+var svgSprite = require('gulp-svg-sprite');
+
+var project = {
+  spriteSource: 'src/*',
+  spriteDestination: 'images/sprite.svg',
+  spriteTemplate: 'tpl/sprite-template.scss',
+  spriteScssDestination: 'scss/lib/_sprite.scss'
+};
+
+/**
+ * @task generate-svg-sprite
+ * Generate the SVG sprite.
+ */
+gulp.task('generate-svg-sprite', function() {
+  return gulp.src(project.spriteSource)
+  .pipe(debug({title: 'unicorn:', minimal: false}))
+  .pipe(svgSprite({
+    shape: {
+      spacing: {
+        padding: 5
+      }
+    },
+    mode: {
+      css: {
+        dest: "./",
+        layout: "diagonal",
+        sprite: project.spriteDestination,
+        bust: false,
+        render: {
+          scss: {
+            dest: project.spriteScssDestination,
+            template: project.spriteTemplate
+          }
+        }
+      }
+    },
+    variables: {
+      mapname: "icons"
+    }
+  }))
+  .pipe(gulp.dest('./'));
+});
+
+/**
+ * @task watch
+ * Watch files and generate the sprite.
+ */
+gulp.task('watch', ['generate-svg-sprite'], function () {
+  gulp.watch(project.spriteSource, ['generate-svg-sprite']);
+});
+
+/**
+ * @task default
+ * Default task so we can call gulp to use the watch task.
+ */
+gulp.task('default', ['watch']);
